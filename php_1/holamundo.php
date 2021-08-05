0feb92e8f80f03e76acc22dcd1910153ff414786
Francisco Saenz <canteko@gmail.com>
2021-08-05 14:00:39 +0200

    hola mundo

---
+++ php_1/holamundo.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prueba PHP</title>
+</head>
+
+<body>
+    <?php
+        // Mi "primer" fichero PHP
+        echo ("Hola Mundo");
+
+        /* 
+            Este programa muetra por pantalla
+            el texto Hola Mundo
+        */
+    ?>
+</body>
+
+</html>
